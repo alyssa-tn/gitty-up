eebc2f586b907c44ead07f241d3357aabcfb76d7
Alyssa Hursh <alyhursh@amazon.com>
2021-11-28 14:34:03 -0800

    Put return statements on new line

--- app/src/main/java/gitty/up/Calculator.java
+++ app/src/main/java/gitty/up/Calculator.java
@@ -2,22 +2,31 @@ package gitty.up;
 
 public class Calculator {
 
-    public static int add(int first, int second) { return first + second; }
+    public static int add(int first, int second) {
+        return first + second;
+    }
 
     public static int add(int first, int second, int third) {
         return first + second + third;
     }
 
-    public static int subtract(int first, int second) { return first - second; }
+    public static int subtract(int first, int second) {
+        return first - second;
+    }
 
-    public static int multiply(int first, int second) { return first * second; }
+    public static int multiply(int first, int second) {
+        return first * second;
+    }
 
     public static int multiply(int first, int second, int third) {
         return first * second * third;
     }
 
-    public static int divide(int first, int second) { return first / second; }
+    public static int divide(int first, int second) {
+        return first / second;
+    }
 
-    public static int mod(int first, int second) { return first % second; }
-
+    public static int mod(int first, int second) {
+        return first % second;
+    }
 }
