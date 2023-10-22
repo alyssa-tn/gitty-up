470bb354b216d07da63a2ae6bace93003d3d50ce
alyssa-tn <tn.alyssa@gmail.com>
2023-10-22 17:34:05 -0500

    Implement add method
    Implement subtract method

--- app/src/main/java/gitty/up/Calculator.java
+++ app/src/main/java/gitty/up/Calculator.java
@@ -2,31 +2,7 @@ package gitty.up;
 
 public class Calculator {
 
-    public static int add(int first, int second) {
-        return first + second;
-    }
+    public static int add(int a, int b) { return a + b; }
 
-    public static int add(int first, int second, int third) {
-        return first + second + third;
-    }
-
-    public static int subtract(int first, int second) {
-        return first - second;
-    }
-
-    public static int multiply(int first, int second) {
-        return first * second;
-    }
-
-    public static int multiply(int first, int second, int third) {
-        return first * second * third;
-    }
-
-    public static int divide(int first, int second) {
-        return first / second;
-    }
-
-    public static int mod(int first, int second) {
-        return first % second;
-    }
+    public static int subtract(int a, int b) { return a - b; }
 }
--- app/src/test/java/gitty/up/CalculatorTest.java
+++ app/src/test/java/gitty/up/CalculatorTest.java
@@ -11,32 +11,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testAddThreeArguments() {
-        assertEquals(6, Calculator.add(1, 2, 3));
-    }
-
-    @Test
     public void testSubtract() {
         assertEquals(1, Calculator.subtract(2, 1));
-    }
-
-    @Test
-    public void testMultiply() {
-        assertEquals(6, Calculator.multiply(2, 3));
-    }
-
-    @Test
-    public void testMultiplyThreeArguments() {
-        assertEquals(30, Calculator.multiply(2, 3, 5));
-    }
-
-    @Test
-    public void testDivide() {
-        assertEquals(2, Calculator.divide(6, 3));
-    }
-
-    @Test
-    public void testMod() {
-        assertEquals(2, Calculator.divide(7, 3));
     }
 }
